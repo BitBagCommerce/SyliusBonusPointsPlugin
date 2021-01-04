d44c3b37c80bfa5f7b0c621c6f84fffdcbf4166f
bartekTw <bartosz.twardowski@opoczta.pl>
2021-01-04 11:46:33 +0100

    improve BonusPointsApplyValidator

--- src/Validator/Constraints/BonusPointsApplyValidator.php
+++ src/Validator/Constraints/BonusPointsApplyValidator.php
@@ -4,36 +4,58 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusBonusPointsPlugin\Validator\Constraints;
 
-use BitBag\SyliusBonusPointsPlugin\Entity\BonusPointsAwareInterface;
-use BitBag\SyliusBonusPointsPlugin\Entity\BonusPointsInterface;
+use BitBag\SyliusBonusPointsPlugin\Checker\Eligibility\BonusPointsStrategyEligibilityCheckerInterface;
+use BitBag\SyliusBonusPointsPlugin\Checker\Eligibility\BonusPointsStrategyRulesEligibilityChecker;
 use BitBag\SyliusBonusPointsPlugin\Entity\BonusPointsStrategyInterface;
 use BitBag\SyliusBonusPointsPlugin\Repository\BonusPointsStrategyRepositoryInterface;
-use BitBag\SyliusBonusPointsPlugin\Resolver\BonusPointsResolverInterface;
-use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Sylius\Component\Order\Context\CartContextInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
-use Webmozart\Assert\Assert;
 
 final class BonusPointsApplyValidator extends ConstraintValidator
 {
     /** @var BonusPointsStrategyRepositoryInterface */
     private $bonusPointsStrategyRepository;
 
-    public function __construct(BonusPointsStrategyRepositoryInterface $bonusPointsStrategyRepository)
-    {
+    /** @var BonusPointsStrategyRulesEligibilityChecker */
+    private $bonusPointsStrategyEligibilityChecker;
+
+    /** @var CartContextInterface */
+    private $cartContext;
+
+    public function __construct(
+        BonusPointsStrategyRepositoryInterface $bonusPointsStrategyRepository,
+        BonusPointsStrategyEligibilityCheckerInterface $bonusPointsStrategyEligibilityChecker,
+        CartContextInterface $cartContext
+    ) {
         $this->bonusPointsStrategyRepository = $bonusPointsStrategyRepository;
+        $this->bonusPointsStrategyEligibilityChecker = $bonusPointsStrategyEligibilityChecker;
+        $this->cartContext = $cartContext;
     }
 
     public function validate($bonusPoints, Constraint $constraint): void
     {
-        $bonusPointsStrategies = $this->bonusPointsStrategyRepository->findAll();
+        $bonusPointsStrategies = $this->bonusPointsStrategyRepository->findAllActive();
+
+        $order = $this->cartContext->getCart();
+        $orderItems = $order->getItems();
 
         /** @var BonusPointsStrategyInterface $bonusPointsStrategy */
         foreach ($bonusPointsStrategies as $bonusPointsStrategy) {
-            if ($bonusPointsStrategy->getCalculatorType() === 'per_order_price') {
-                if ($bonusPoints % 100 !== 0 || $bonusPoints < 100) {
-                    $this->context->buildViolation($constraint->message)->setTranslationDomain('validators')->addViolation();
+            foreach ($orderItems as $orderItem) {
+                if (!$this->bonusPointsStrategyEligibilityChecker->isEligible($orderItem, $bonusPointsStrategy)) {
+                    $this->context
+                        ->buildViolation('bitbag_sylius_bonus_points.cart.bonus_points.invalid_taxon')
+                        ->setTranslationDomain('validators')
+                        ->addViolation()
+                    ;
+
+                    return;
                 }
+            }
+
+            if ($bonusPoints % 100 !== 0 || $bonusPoints < 100) {
+                $this->context->buildViolation($constraint->message)->setTranslationDomain('validators')->addViolation();
             }
         }
     }
