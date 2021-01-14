78a0c3d0b49b359940f3d19e5c6c280e3a2309f7
bartekTw <bartosz.twardowski@opoczta.pl>
2021-01-14 11:23:58 +0100

    fix phpspec test

--- spec/Validator/Constraints/BonusPointsApplyValidatorSpec.php
+++ spec/Validator/Constraints/BonusPointsApplyValidatorSpec.php
@@ -13,6 +13,8 @@ use BitBag\SyliusBonusPointsPlugin\Validator\Constraints\BonusPointsApplyValidat
 use Doctrine\Common\Collections\ArrayCollection;
 use PhpSpec\ObjectBehavior;
 use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Model\OrderItem;
+use Sylius\Component\Core\Model\OrderItemInterface;
 use Sylius\Component\Order\Context\CartContextInterface;
 use Symfony\Component\Validator\ConstraintValidator;
 
@@ -29,6 +31,7 @@ final class BonusPointsApplyValidatorSpec extends ObjectBehavior
     function it_is_initializable(): void
     {
         $this->shouldHaveType(BonusPointsApplyValidator::class);
+        $this->shouldBeAnInstanceOf(ConstraintValidator::class);
     }
 
     function it_extends_constraint_validator_class(): void
@@ -40,21 +43,26 @@ final class BonusPointsApplyValidatorSpec extends ObjectBehavior
         BonusPointsStrategyRepositoryInterface $bonusPointsStrategyRepository,
         BonusPointsStrategyInterface $bonusPointsStrategy,
         CartContextInterface $cartContext,
-        OrderInterface $order
+        OrderInterface $order,
+        BonusPointsStrategyEligibilityCheckerInterface $bonusPointsStrategyEligibilityChecker
     ): void {
         $bonusPointsApplyConstraint = new BonusPointsApply();
+        $orderItem = new OrderItem();
 
-        $bonusPointsStrategies = [];
+        $bonusPointsStrategies = new ArrayCollection([$bonusPointsStrategy]);
+        $orderItems = new ArrayCollection([$orderItem]);
 
-        $bonusPointsStrategyRepository->findActiveByCalculatorType(PerOrderPriceCalculator::TYPE)->willReturn($bonusPointsStrategies);
+        $bonusPointsStrategyRepository->findActiveByCalculatorType(PerOrderPriceCalculator::TYPE)->willReturn($bonusPointsStrategies->toArray());
         $cartContext->getCart()->willReturn($order);
-        $order->getItems()->willReturn(new ArrayCollection());
-        
+        $order->getItems()->willReturn($orderItems);
+        $order->getItems()->willReturn($orderItems);
+        $bonusPointsStrategyEligibilityChecker->isEligible($orderItem, $bonusPointsStrategy)->willReturn(true);
+
         $bonusPointsStrategyRepository->findActiveByCalculatorType(PerOrderPriceCalculator::TYPE)->shouldBeCalled();
         $cartContext->getCart()->shouldBeCalled();
         $order->getItems()->shouldBeCalled();
-        $bonusPointsStrategy->getCalculatorType()->shouldNotBeCalled();
+        $bonusPointsStrategyEligibilityChecker->isEligible($orderItem, $bonusPointsStrategy)->shouldBeCalled();
 
-        $this->validate(99, $bonusPointsApplyConstraint);
+        $this->validate(100, $bonusPointsApplyConstraint);
     }
 }
