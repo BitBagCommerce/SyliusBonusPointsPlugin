a11299218eedd02d3b678d66cb46555e1708979a
bartekTw <bartosz.twardowski@opoczta.pl>
2021-02-05 12:35:46 +0100

    add DelegatingBonusPointsStrategyCalculatorSpec

---
+++ spec/Calculator/DelegatingBonusPointsStrategyCalculatorSpec.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\BitBag\SyliusBonusPointsPlugin\Calculator;
+
+use BitBag\SyliusBonusPointsPlugin\Calculator\BonusPointsStrategyCalculatorInterface;
+use BitBag\SyliusBonusPointsPlugin\Calculator\DelegatingBonusPointsStrategyCalculator;
+use BitBag\SyliusBonusPointsPlugin\Calculator\DelegatingBonusPointsStrategyCalculatorInterface;
+use BitBag\SyliusBonusPointsPlugin\Calculator\PerOrderPriceCalculator;
+use BitBag\SyliusBonusPointsPlugin\Checker\Eligibility\BonusPointsStrategyEligibilityCheckerInterface;
+use BitBag\SyliusBonusPointsPlugin\Entity\BonusPointsStrategyInterface;
+use PhpSpec\ObjectBehavior;
+use Sylius\Component\Core\Model\OrderItemInterface;
+use Sylius\Component\Registry\ServiceRegistryInterface;
+
+final class DelegatingBonusPointsStrategyCalculatorSpec extends ObjectBehavior
+{
+    function let(
+        ServiceRegistryInterface $registry,
+        BonusPointsStrategyEligibilityCheckerInterface $bonusPointsStrategyEligibilityChecker
+    ): void {
+        $this->beConstructedWith($registry, $bonusPointsStrategyEligibilityChecker);
+    }
+
+    function it_is_initializable(): void
+    {
+        $this->shouldHaveType(DelegatingBonusPointsStrategyCalculator::class);
+    }
+
+    function it_implements_bonus_points_resolver_interface(): void
+    {
+        $this->shouldHaveType(DelegatingBonusPointsStrategyCalculatorInterface::class);
+    }
+
+    function it_calculates(
+        ServiceRegistryInterface $registry,
+        BonusPointsStrategyEligibilityCheckerInterface $bonusPointsStrategyEligibilityChecker,
+        BonusPointsStrategyInterface $bonusPointsStrategy,
+        OrderItemInterface $orderItem,
+        BonusPointsStrategyCalculatorInterface $calculator
+    ): void {
+        $bonusPointsStrategy->getCalculatorType()->willReturn(PerOrderPriceCalculator::TYPE);
+        $registry->get(PerOrderPriceCalculator::TYPE)->willReturn($calculator);
+        $bonusPointsStrategyEligibilityChecker->isEligible($orderItem, $bonusPointsStrategy)->willReturn(true);
+        $bonusPointsStrategy->getCalculatorConfiguration()->willReturn(['numberOfPointsEarnedPerOneCurrency' => 2]);
+        $calculator->calculate($orderItem, ['numberOfPointsEarnedPerOneCurrency' => 2], 0)->willReturn(88);
+
+        $bonusPointsStrategy->getCalculatorType()->shouldBeCalled();
+        $registry->get(PerOrderPriceCalculator::TYPE)->shouldBeCalled();
+        $bonusPointsStrategyEligibilityChecker->isEligible($orderItem, $bonusPointsStrategy)->shouldBeCalled();
+        $bonusPointsStrategy->getCalculatorConfiguration()->shouldBeCalled();
+        $calculator->calculate($orderItem, ['numberOfPointsEarnedPerOneCurrency' => 2], 0)->shouldBeCalled();
+
+        $this->calculate($orderItem, $bonusPointsStrategy)->shouldReturn(88);
+    }
+
+    function it_calculates_when_is_no_eligible(
+        ServiceRegistryInterface $registry,
+        BonusPointsStrategyEligibilityCheckerInterface $bonusPointsStrategyEligibilityChecker,
+        BonusPointsStrategyInterface $bonusPointsStrategy,
+        OrderItemInterface $orderItem,
+        BonusPointsStrategyCalculatorInterface $calculator
+    ): void {
+        $bonusPointsStrategy->getCalculatorType()->willReturn(PerOrderPriceCalculator::TYPE);
+        $registry->get(PerOrderPriceCalculator::TYPE)->willReturn($calculator);
+        $bonusPointsStrategyEligibilityChecker->isEligible($orderItem, $bonusPointsStrategy)->willReturn(false);
+
+        $bonusPointsStrategy->getCalculatorType()->shouldBeCalled();
+        $registry->get(PerOrderPriceCalculator::TYPE)->shouldBeCalled();
+        $bonusPointsStrategyEligibilityChecker->isEligible($orderItem, $bonusPointsStrategy)->shouldBeCalled();
+
+        $this->calculate($orderItem, $bonusPointsStrategy)->shouldReturn(0);
+    }
+
+    function it_returns_true_if_calculator_is_per_order_item(
+        ServiceRegistryInterface $registry,
+        BonusPointsStrategyInterface $bonusPointsStrategy,
+        BonusPointsStrategyCalculatorInterface $calculator
+    ): void {
+        $bonusPointsStrategy->getCalculatorType()->willReturn(PerOrderPriceCalculator::TYPE);
+        $registry->get(PerOrderPriceCalculator::TYPE)->willReturn($calculator);
+        $calculator->isPerOrderItem()->willReturn(true);
+
+        $bonusPointsStrategy->getCalculatorType()->shouldBeCalled();
+        $registry->get(PerOrderPriceCalculator::TYPE)->shouldBeCalled();
+        $calculator->isPerOrderItem()->shouldBeCalled();
+
+        $this->isPerOrderItem($bonusPointsStrategy)->shouldReturn(true);
+    }
+
+    function it_returns_true_if_calculator_is_not_per_order_item(
+        ServiceRegistryInterface $registry,
+        BonusPointsStrategyInterface $bonusPointsStrategy,
+        BonusPointsStrategyCalculatorInterface $calculator
+    ): void {
+        $bonusPointsStrategy->getCalculatorType()->willReturn(PerOrderPriceCalculator::TYPE);
+        $registry->get(PerOrderPriceCalculator::TYPE)->willReturn($calculator);
+        $calculator->isPerOrderItem()->willReturn(false);
+
+        $bonusPointsStrategy->getCalculatorType()->shouldBeCalled();
+        $registry->get(PerOrderPriceCalculator::TYPE)->shouldBeCalled();
+        $calculator->isPerOrderItem()->shouldBeCalled();
+
+        $this->isPerOrderItem($bonusPointsStrategy)->shouldReturn(false);
+    }
+}
