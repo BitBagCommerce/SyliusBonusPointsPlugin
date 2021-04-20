e174b9e0228d71e343ab3252edf96d5534c32afd
bartekTw <bartosz.twardowski@opoczta.pl>
2021-04-20 09:24:19 +0200

    improve points purifying

--- src/Purifier/OrderBonusPointsPurifierInterface.php
+++ src/Purifier/OrderBonusPointsPurifierInterface.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace BitBag\SyliusBonusPointsPlugin\Purifier;
 
 use BitBag\SyliusBonusPointsPlugin\Entity\BonusPointsInterface;
+use BitBag\SyliusBonusPointsPlugin\Entity\CustomerBonusPointsInterface;
 
 interface OrderBonusPointsPurifierInterface
 {
-    public function purify(BonusPointsInterface $bonusPoints): void;
+    public function purify(
+        BonusPointsInterface $bonusPoints,
+        CustomerBonusPointsInterface $customerBonusPoints = null
+    ): void;
 }
