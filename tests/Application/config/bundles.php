ac279ee647d8915fa85128e580d78e8b2ef488ad
Tomasz Grochowski <tomasz.grochowski@bitbag.pl>
2021-06-29 08:26:55 +0200

    Configuration change

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -58,5 +58,4 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sylius\Bundle\ApiBundle\SyliusApiBundle::class => ['all' => true],
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
-    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true]
 ];
