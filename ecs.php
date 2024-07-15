3d4232051d8fe0e9a38e23034677ba8133b98cf8
janpalen <jan.palen@bitbag.pl>
2024-07-15 14:53:29 +0200

    OP-376 - Fixes

--- ecs.php
+++ ecs.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $config): void {
-
-    // Change this if you want to automatically add BitBag OpenSource Header
     putenv('ALLOW_BITBAG_OS_HEADER=1');
 
     $config->import('vendor/bitbag/coding-standard/ecs.php');
